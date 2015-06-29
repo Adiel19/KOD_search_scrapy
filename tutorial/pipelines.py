fec969fba3048f4422bcf47f96e3e1ec7cc189be
Adiel19 <adielgranados.19@gmail.com>
2015-06-29 18:31:37 -0500

    It worked

--- tutorial/pipelines.py
+++ tutorial/pipelines.py
@@ -11,10 +11,10 @@ import codecs
 class JsonWithEncodingPipeline(object):
 
     def __init__(self):
-        self.file = codecs.open('scraped_data_utf8.json', 'w', encoding='utf-8')
+        self.file = codecs.open('lit/scraped_data_utf8.json', 'w', encoding='utf-8')
 
     def process_item(self, item, spider):
-        line = json.dumps(dict(item), ensure_ascii=False) + "\n"    
+        line = json.dumps(dict(item), ensure_ascii=False) + "\n\n"    
         self.file.write(line)
        
         return item
--- tutorial/spiders/kod_archive.py
+++ tutorial/spiders/kod_archive.py
@@ -2,13 +2,18 @@ import scrapy
 
 from tutorial.items import KODItem
 
+
+file_lit = open("lit/lit_not_spanish.txt", "r")
+lit_not_spanish = file_lit.read()
+file_lit.close()
+
 class KODSpider(scrapy.Spider):
     name = "kod_archive"
     allowed_domains = ["thetrumpet.com"]
     start_urls = [
             "https://www.thetrumpet.com/key_of_david/program_archive?page=1"
             ]
-        
+
     def parse(self, response):
         for href in response.xpath("//div[@class='title']/a/@href"):
             url = response.urljoin(href.extract())
@@ -23,12 +28,34 @@ class KODSpider(scrapy.Spider):
             yield scrapy.Request(url_next, callback = self.parse)
 
     def parse_dir_contents(self, response):
-#        for sel in response.xpath("//div[@class='content']/div[starts-with(@class, 'title')]"):
-#            item = KODItem()
-#            item['title'] = sel.xpath("a/text()").extract()
-#            item['link'] = sel.xpath("a/@href").extract()
-#            item['lit'] = []
+
+        for sel in response.xpath("//div[@class='content']/div[starts-with(@class, 'title')]"):
+            if response.xpath("//div[@class='not_available']"):
+                print "**NOT AVAILABLE**"
+                continue
+            item = KODItem()
+            item['title'] = sel.xpath("a/text()").extract()
+            item['link'] = sel.xpath("a/@href").extract()
+            item['lit'] = self.compare_lit(response)
 #            for lit in response.xpath("//div[@class='offer available']/div[@class='title']"):                
-#                item['lit'].append(lit.xpath("a/text()").extract())           
+#                item['lit'].append(lit.xpath("a/text()").extract())
+#                print item['lit']
 #            yield item
-        pass
+#
+            if item['lit'] == -1:
+                continue
+            else:
+                yield item
+
+    def compare_lit(self, response):
+        lits = []
+        for lit in response.xpath("//div[@class='offer available']/div[@class='title']"):
+            if lit.xpath("a/text()").extract()[0].encode('utf-8') in lit_not_spanish:
+                return -1
+            elif "DVD" in lit.xpath("a/text()").extract()[0].encode('utf-8'):
+                continue
+            else:
+                lits.append(lit.xpath("a/text()").extract()[0])
+        return lits
+            
+
